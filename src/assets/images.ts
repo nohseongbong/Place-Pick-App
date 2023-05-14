ba55889ae5a346eaba65e96002e3c29b8fa17059
shtjdqhd <shtjdqhd@naver.com>
2023-05-14 19:05:01 +0900

    style: 이미지 추가

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,5 +1,5 @@
 export const IMG = {
-  FOOD_CATEGORY: require('../assets/images/icon/folk-knife.png'),
+  // FOOD_CATEGORY: require('../assets/images/icon/folk-knife.png'),
 };
 
 import LOGO from '../assets/images/svg/logo.svg';
@@ -7,6 +7,16 @@ import HOME_ON from '../assets/images/svg/tab_home_on.svg';
 import HOME_OFF from '../assets/images/svg/tab_home_off.svg';
 import COLLECTION_ON from '../assets/images/svg/tab_collection_on.svg';
 import COLLECTION_OFF from '../assets/images/svg/tab_collection_off.svg';
+import CATEGORY_BAR from '../assets/images/svg/category_bar.svg';
+import CATEGORY_CAFE from '../assets/images/svg/category_cafe.svg';
+import CATEGORY_FLAG from '../assets/images/svg/category_flag.svg';
+import CATEGORY_SHOP from '../assets/images/svg/category_shop.svg';
+import CATEGORY_PARK from '../assets/images/svg/category_park.svg';
+import CATEGORY_RESTAURANT from '../assets/images/svg/category_restaurant.svg';
+import COURSE_1 from '../assets/images/svg/course_1.svg';
+import COURSE_2 from '../assets/images/svg/course_2.svg';
+import COURSE_3 from '../assets/images/svg/course_3.svg';
+import COURSE_4 from '../assets/images/svg/course_4.svg';
 
 export const SVG_IMG = {
   LOGO,
@@ -14,4 +24,14 @@ export const SVG_IMG = {
   COLLECTION_ON,
   HOME_OFF,
   COLLECTION_OFF,
+  CATEGORY_BAR,
+  CATEGORY_CAFE,
+  CATEGORY_FLAG,
+  CATEGORY_SHOP,
+  CATEGORY_PARK,
+  CATEGORY_RESTAURANT,
+  COURSE_1,
+  COURSE_2,
+  COURSE_3,
+  COURSE_4,
 };
