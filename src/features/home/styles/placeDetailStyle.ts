750cabe175847de50a26fe14b970db957af93fba
shtjdqhd <shtjdqhd@naver.com>
2023-05-21 18:23:27 +0900

    style: 바텀시트 플레이스 디테일 스타일 수정

--- src/features/home/styles/placeDetailStyle.ts
+++ src/features/home/styles/placeDetailStyle.ts
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
-import {palette} from '../../../shared/constants/palette';
-import {ht, wt} from '../../../lib/responsiveSize';
+import {fontWt, palette} from '../../../shared/constants/palette';
+import {fs, ht, wt} from '../../../lib/responsiveSize';
 
 const style = () => {
   return StyleSheet.create({
@@ -8,13 +8,67 @@ const style = () => {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
+      paddingHorizontal: wt(20),
+      backgroundColor: palette.BACKGROUND,
     },
-    test_PR_btn: {
-      width: wt(150),
-      height: ht(50),
-      backgroundColor: palette.PRIMARY,
+    place_info_container: {
+      width: '100%',
+      alignItems: 'flex-start',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    place_info_wrap: {
+      width: '76%',
+      height: '100%',
+      marginLeft: wt(12),
+      overflow: 'hidden',
+    },
+    place_info_name_text: {
+      fontSize: fs(18),
+      fontFamily: fontWt.Bold,
+      color: palette.TEXT,
+    },
+    place_info_address_text: {
+      fontSize: fs(14),
+      color: palette.TEXT,
+      marginTop: ht(8),
+      marginBottom: ht(4),
+    },
+    place_info_rating_wrap: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      gap: wt(6),
+    },
+    place_info_rating_text: {
+      fontSize: fs(14),
+      color: palette.TEXT,
+    },
+    close_wrap: {
+      width: wt(30),
+      height: ht(30),
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    btns_wrap: {
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: ht(23),
+      justifyContent: 'space-around',
+    },
+    btn_wrap: {
+      width: '48%',
+      maxWidth: wt(250),
+      height: ht(36),
+      borderRadius: 60,
       justifyContent: 'center',
       alignItems: 'center',
+      backgroundColor: palette.INNER_BACKGOUND,
+    },
+    btn_text: {
+      fontSize: fs(14),
+      color: palette.TEXT,
+      fontFamily: fontWt.SemiBold,
     },
   });
 };
