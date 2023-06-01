8fd008d6df2e5c241f393ea15d5fd3eec51821cf
nohseongbong <shtjdqhd@naver.com>
2023-06-02 00:49:00 +0900

    style: 검색 컴포넌트 작업중

--- src/features/home/components/bottomSheetContents/PlaceSearch.tsx
+++ src/features/home/components/bottomSheetContents/PlaceSearch.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Image, View} from 'react-native';
 
 import CustomText from '../../../../shared/components/customComponents/CustomText';
 import CustomTouchable from '../../../../shared/components/customComponents/CustomTouchable';
 import {observer} from 'mobx-react-lite';
 import {placeDetailStore} from '../../store/placeDetailStore';
 import {bottomSheetStore} from '../../store/bottomSheetStore';
-import {SVG_IMG} from '../../../../assets/images';
+import {IMG, SVG_IMG} from '../../../../assets/images';
 import {palette} from '../../../../shared/constants/palette';
 import style from '../../styles/placeSearchStyle';
 import CustomTextInput from '../../../../shared/components/customComponents/CustomTextInput';
@@ -16,8 +16,19 @@ const PlaceSearch = observer(() => {
 
   return (
     <View style={styles.container}>
-      <View>
-        <CustomTextInput />
+      <View style={styles.search_bar_wrap}>
+        <View style={styles.search_text_wrap}>
+          <CustomTouchable style={styles.search_btn}>
+            <SVG_IMG.SEARCH width={16} height={16} />
+          </CustomTouchable>
+          <CustomTextInput style={styles.search_input} keyboardType="web-search" placeholder="검색" />
+          <CustomTouchable style={styles.search_remove_btn}>
+            <SVG_IMG.SEARCH_REMOVE width={16} height={16} />
+          </CustomTouchable>
+        </View>
+        <CustomTouchable style={styles.close_btn}>
+          <CustomText style={styles.close_text}>취소</CustomText>
+        </CustomTouchable>
       </View>
     </View>
   );
--- src/features/home/styles/placeSearchStyle.ts
+++ src/features/home/styles/placeSearchStyle.ts
@@ -11,6 +11,44 @@ const style = () => {
       paddingHorizontal: wt(20),
       backgroundColor: palette.BACKGROUND,
     },
+    search_bar_wrap: {
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    search_text_wrap: {
+      width: '88%',
+      height: ht(37),
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      backgroundColor: palette.INNER_BACKGOUND,
+      borderRadius: 32,
+      paddingHorizontal: wt(10),
+    },
+    search_input: {
+      width: '100%',
+      borderWidth: 0,
+      paddingHorizontal: wt(20),
+    },
+    search_btn: {
+      position: 'absolute',
+      left: wt(10),
+    },
+    search_remove_btn: {
+      position: 'absolute',
+      right: wt(10),
+    },
+    close_btn: {
+      width: wt(35),
+      height: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    close_text: {
+      fontSize: fs(12),
+    },
   });
 };
 
