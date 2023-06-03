a423eac4e81be300049481df49110412d5e72f94
nohseongbong <shtjdqhd@naver.com>
2023-06-03 16:05:53 +0900

    refactor: 타입 수정

---
+++ src/features/home/constants/searchTabType.ts
@@ -0,0 +1,16 @@
+export const searchTabType = {
+  ALL: '전체',
+  BAR: '바',
+  PARK: '공원',
+  RESTAURANT: '음식점',
+  CAFE: '카페',
+  POINT_OF_INTEREST: '가볼만한곳',
+  STORE: '가게',
+};
+
+export const tabList = [
+  searchTabType.ALL,
+  searchTabType.RESTAURANT,
+  searchTabType.CAFE,
+  searchTabType.POINT_OF_INTEREST,
+];
