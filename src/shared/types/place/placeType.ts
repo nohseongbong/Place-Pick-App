6aae845003280e97f4118c97974e07e7b966f29e
nohseongbong <shtjdqhd@naver.com>
2023-06-03 16:06:10 +0900

    refactor: place type 수정

--- src/shared/types/place/placeType.ts
+++ src/shared/types/place/placeType.ts
@@ -1,11 +1,13 @@
+import {PlaceCategoryType} from '../../constants/placeCategoryType';
+
 export interface PlaceType {
   place_id: string;
   name: string;
   formatted_address: string;
   rating: number;
   user_ratings_total: number;
-  //   types: string[];
-  category: string;
+  // type: PlaceCategoryType;
+  category: PlaceCategoryType;
   location: {
     lat: number;
     lng: number;
