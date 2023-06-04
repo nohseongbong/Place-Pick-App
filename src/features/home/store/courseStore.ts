557d6fec932c9e4056844c88d5d902f2c1dbb03a
nohseongbong <shtjdqhd@naver.com>
2023-06-04 19:04:39 +0900

    feat: 코스 스토어에 선택된 코드 리스트 함수 추가

--- src/features/home/store/courseStore.ts
+++ src/features/home/store/courseStore.ts
@@ -1,14 +1,43 @@
 import {makeAutoObservable, runInAction} from 'mobx';
-import CoursePlaceModel from './coursePlaceModel';
+import {PlaceType} from '../../../shared/types/place/placeType';
+import {ConnectType} from '../types/ConnectType';
 
 class CourseStore {
   courseNumber: number = 1;
-  courseList: CoursePlaceModel[] = [];
+  courseList: PlaceType[] = [];
+  courseConectList: ConnectType[] = [];
+
   constructor() {
     makeAutoObservable(this);
   }
   addCourseNumber = () => {
     this.courseNumber = this.courseNumber + 1;
+  };
+
+  setAddCourseList = (place: PlaceType) => {
+    this.courseList.push(place);
+    let arr: ConnectType[] = [];
+    if (this.courseList.length > 1) {
+      this.courseList.map((item, index) => {
+        if (this.courseList.length - 1 !== index) {
+          const start = item;
+          const end = this.courseList[index + 1];
+          const connectObj: ConnectType = {
+            start: {
+              place_id: start.place_id,
+              location: start.location,
+            },
+            end: {
+              place_id: end.place_id,
+              location: end.location,
+            },
+          };
+          arr.push(connectObj);
+          return;
+        }
+      });
+      this.courseConectList = arr;
+    }
   };
 }
 
