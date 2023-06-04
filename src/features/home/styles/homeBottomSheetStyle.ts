ba1a6411770de59d0b19e80f3ab49c2e2fc350b9
nohseongbong <shtjdqhd@naver.com>
2023-06-04 19:07:58 +0900

    style: 바텀시트 스타일 수정

--- src/features/home/styles/homeBottomSheetStyle.ts
+++ src/features/home/styles/homeBottomSheetStyle.ts
@@ -19,6 +19,18 @@ const style = () => {
             shadowOffset: {width: 0, height: -4},
             shadowOpacity: 0.2,
           },
+    handler_wrap: {
+      width: '100%',
+      height: ht(40),
+      alignItems: 'center',
+      paddingTop: ht(10),
+    },
+    handler: {
+      width: wt(30),
+      height: ht(3),
+      backgroundColor: 'black',
+      borderRadius: 20,
+    },
   });
 };
 
