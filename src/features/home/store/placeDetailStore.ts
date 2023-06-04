2acf4a31099195e5962dfbdb656a8bfa6e41ce48
nohseongbong <shtjdqhd@naver.com>
2023-06-04 19:06:47 +0900

    refactor: 플레이스 디테일 스토어에 데이터 연결작업

--- src/features/home/store/placeDetailStore.ts
+++ src/features/home/store/placeDetailStore.ts
@@ -1,9 +1,40 @@
 import {makeAutoObservable, runInAction} from 'mobx';
 import {api} from '../../../shared/api/google/api';
+import {PlaceCategoryType} from '../../../shared/constants/placeCategoryType';
+import {_categoryType} from '../../../shared/utils/placeCategory';
+import {PlaceType} from '../../../shared/types/place/placeType';
 
 class PlaceDetailStore {
+  place_id: string = '';
+  name: string = '';
+  formatted_address: string = '';
+  rating: number = 0;
+  user_ratings_total: number = 0;
+  url: string = '';
+  category: PlaceCategoryType = PlaceCategoryType.BAR;
+  location: {
+    latitude: number;
+    longitude: number;
+  } = {
+    latitude: 0,
+    longitude: 0,
+  };
+
   constructor() {
     makeAutoObservable(this);
+  }
+
+  get getPlaceInfo(): PlaceType {
+    return {
+      place_id: this.place_id,
+      name: this.name,
+      formatted_address: this.formatted_address,
+      rating: this.rating,
+      user_ratings_total: this.user_ratings_total,
+      url: this.url,
+      category: this.category,
+      location: this.location,
+    };
   }
 
   fetchPlaceDetail = async (place_id: string) => {
@@ -12,7 +43,14 @@ class PlaceDetailStore {
       if (response.status !== 200) {
         return;
       }
-      const result = response.data.result;
+      const data = response.data.result;
+      runInAction(() => {
+        Object.assign(this, {
+          ...data,
+          category: _categoryType(data.types),
+          location: {latitude: data.geometry.location.latitude, longitude: data.geometry.location.longitude},
+        });
+      });
     } catch (error) {
       console.log(error);
       return [];
