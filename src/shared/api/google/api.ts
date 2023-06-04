2e6e998cb00e5a544e614dcdab61b877526129d1
nohseongbong <shtjdqhd@naver.com>
2023-06-04 19:01:48 +0900

    refactor: place detail google api 필터 값 수정

--- src/shared/api/google/api.ts
+++ src/shared/api/google/api.ts
@@ -16,7 +16,7 @@ export class Api {
     axiosInstanceGoogleApi.get('/place/details/json', {
       params: {
         place_id: place_id,
-        fields: 'name,formatted_address,rating,user_ratings_total,url,place_id',
+        fields: 'name,formatted_address,rating,user_ratings_total,url,place_id,types,geometry',
       },
     });
   searchGooglePlaces = ({query, type}: GooglePlaceSearchReq): Promise<GooglePlaceSearchRes> =>
