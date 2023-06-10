4ec6fd9ca64ac93bbd94a389b1bb8857148c06f7
nohseongbong <shtjdqhd@naver.com>
2023-06-10 17:46:15 +0900

    feat: 검색 기능 추가

--- src/features/home/components/bottomSheetContents/PlaceSearch.tsx
+++ src/features/home/components/bottomSheetContents/PlaceSearch.tsx
@@ -1,16 +1,19 @@
-import React, {useEffect, useRef} from 'react';
-import {Image, TextInput, View} from 'react-native';
+import React, {memo, useRef} from 'react';
+import {TextInput, View} from 'react-native';
 
 import CustomText from '../../../../shared/components/customComponents/CustomText';
 import CustomTouchable from '../../../../shared/components/customComponents/CustomTouchable';
 import {observer} from 'mobx-react-lite';
-import {IMG, SVG_IMG} from '../../../../assets/images';
+import {SVG_IMG} from '../../../../assets/images';
 import style from '../../styles/placeSearchStyle';
 import CustomTextInput from '../../../../shared/components/customComponents/CustomTextInput';
-import CustomFlatList from '../../../../shared/components/customComponents/CustomFlatList';
 import {searchStore} from '../../store/searchStore';
 import {tabList} from '../../constants/searchTabType';
+import {PlaceType} from '../../../../shared/types/place/placeType';
 import {bottomSheetStore} from '../../store/bottomSheetStore';
+import {FocusedType} from '../../constants/bottomSheetFocusedType';
+import {BottomSheetScrollView} from '@gorhom/bottom-sheet';
+import {placeDetailStore} from '../../store/placeDetailStore';
 
 const PlaceSearch = observer(() => {
   const styles = style();
@@ -23,29 +26,53 @@ const PlaceSearch = observer(() => {
     searchStore.setIsFocusSearch(false);
   };
 
-  const SearchListItem = () => {
+  const onPressRemoveSearchText = () => {
+    searchStore.setSearchText('');
+  };
+
+  const onPressCancleSearch = () => {
+    searchStore.resetSearch();
+    bottomSheetStore.setFocusedType(FocusedType.CREATE);
+  };
+
+  const SearchListItem = memo(({place}: {place: PlaceType}) => {
+    const onPressPlace = () => {
+      placeDetailStore.fetchPlaceDetail(place.place_id);
+    };
+
+    const CategoryIconView = () => {
+      const category: {[key: string]: JSX.Element} = {
+        restaurant: <SVG_IMG.CATEGORY_RESTAURANT width={36} height={36} />,
+        bar: <SVG_IMG.CATEGORY_BAR width={36} height={36} />,
+        park: <SVG_IMG.CATEGORY_PARK width={36} height={36} />,
+        store: <SVG_IMG.CATEGORY_SHOP width={36} height={36} />,
+        cafe: <SVG_IMG.CATEGORY_CAFE width={36} height={36} />,
+        transit_station: <SVG_IMG.CATEGORY_TRAIN width={36} height={36} />,
+        point_of_interest: <SVG_IMG.CATEGORY_FLAG width={36} height={36} />,
+      };
+      return category[place.category];
+    };
     return (
-      <CustomTouchable style={styles.item}>
+      <CustomTouchable onPress={onPressPlace} style={styles.item}>
         <View style={styles.item_img_wrap}>
-          <SVG_IMG.CATEGORY_RESTAURANT width={36} height={36} />
+          <CategoryIconView />
         </View>
         <View style={styles.item_info_wrap}>
-          <CustomText style={styles.item_name}>땀땀</CustomText>
+          <CustomText style={styles.item_name}>{place.name}</CustomText>
           <View style={styles.item_adress}>
-            <CustomText style={styles.item_text}>강남구 역삼동 ·</CustomText>
-            <CustomText style={styles.item_text}>카페,디저트</CustomText>
+            <CustomText style={styles.item_text}>{place.formatted_address}</CustomText>
+            {/* <CustomText style={styles.item_text}>카페,디저트</CustomText> */}
           </View>
           <View style={styles.item_rating}>
             <SVG_IMG.STAR width={10} height={10} />
-            <CustomText style={styles.item_rating_text}>4.1</CustomText>
+            <CustomText style={styles.item_rating_text}>{place.rating ?? 0}</CustomText>
             <SVG_IMG.PEOPLE width={12} height={8} />
-            <CustomText style={styles.item_rating_text}>1,212</CustomText>
+            <CustomText style={styles.item_rating_text}>{place.user_ratings_total ?? 0}</CustomText>
           </View>
         </View>
       </CustomTouchable>
     );
-  };
-
+  });
   const TabComponent = observer(({item}: {item: string}) => {
     const onPressTab = () => {
       searchStore.setActiveTab(item);
@@ -73,12 +100,14 @@ const PlaceSearch = observer(() => {
             style={styles.search_input}
             keyboardType="web-search"
             placeholder="검색"
+            value={searchStore.searchText}
+            onChangeText={searchStore.setSearchText}
           />
-          <CustomTouchable style={styles.search_remove_btn}>
+          <CustomTouchable onPress={onPressRemoveSearchText} style={styles.search_remove_btn}>
             <SVG_IMG.SEARCH_REMOVE width={16} height={16} />
           </CustomTouchable>
         </View>
-        <CustomTouchable style={styles.close_btn}>
+        <CustomTouchable onPress={onPressCancleSearch} style={styles.close_btn}>
           <CustomText style={styles.close_text}>취소</CustomText>
         </CustomTouchable>
       </View>
@@ -88,14 +117,11 @@ const PlaceSearch = observer(() => {
           return <TabComponent key={`${item}_${index}`} item={item} />;
         })}
       </View>
-      <CustomFlatList
-        style={styles.list_wrap}
-        data={searchStore.searchList}
-        keyExtractor={item => item.place_id}
-        renderItem={() => {
-          return <SearchListItem />;
-        }}
-      />
+      <BottomSheetScrollView contentContainerStyle={styles.list_wrap}>
+        {searchStore.searchList.map(item => {
+          return <SearchListItem place={item} key={item.place_id} />;
+        })}
+      </BottomSheetScrollView>
     </View>
   );
 });
