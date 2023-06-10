99aa9a0d528656817526edecf5ae7f8a7d402e9a
nohseongbong <shtjdqhd@naver.com>
2023-06-10 17:41:36 +0900

    style: 코스 완성 버트 추가

--- src/features/home/components/HomeContainer.tsx
+++ src/features/home/components/HomeContainer.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
 
 import style from '../styles/homeContainerStyle';
@@ -7,6 +7,12 @@ import HomeBottomSheet from './HomeBottomSheet';
 import {observer} from 'mobx-react-lite';
 import {homeStore} from '../store/homeStore';
 import {MarKerType} from '../../../shared/types/place/markerType';
+import {courseStore} from '../store/courseStore';
+import CustomTouchable from '../../../shared/components/customComponents/CustomTouchable';
+import CustomText from '../../../shared/components/customComponents/CustomText';
+import {bottomSheetStore} from '../store/bottomSheetStore';
+import {FocusedType} from '../constants/bottomSheetFocusedType';
+import {placeDetailStore} from '../store/placeDetailStore';
 
 const HomeContainer = observer(() => {
   const styles = style();
@@ -18,10 +24,19 @@ const HomeContainer = observer(() => {
     setMarkers(markers);
   };
 
+  useEffect(() => {
+    onPressNearPlaceBtn();
+  }, [placeDetailStore.place_id]);
+
   return (
     <View style={styles.container}>
       <HomeMap onPressNearPlaceBtn={onPressNearPlaceBtn} markers={markers} />
       <HomeBottomSheet />
+      {courseStore.courseList.length > 1 && bottomSheetStore.focusedType === FocusedType.CREATE && (
+        <CustomTouchable style={styles.complete_course_wrap}>
+          <CustomText style={styles.complete_course_text}>코스 완성하기</CustomText>
+        </CustomTouchable>
+      )}
     </View>
   );
 });
