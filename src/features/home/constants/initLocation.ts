1ffac4a433b6413c40d0f9434df7880e607da7f8
nohseongbong <shtjdqhd@naver.com>
2023-06-18 16:25:41 +0900

    refactor: place detail 클릭 시 줌 확대 수정

--- src/features/home/constants/initLocation.ts
+++ src/features/home/constants/initLocation.ts
@@ -1,6 +1,6 @@
 export const initLocation = {
   latitude: 37.4979052,
   longitude: 127.0275777,
-  latitudeDelta: 0.01,
-  longitudeDelta: 0.01,
+  latitudeDelta: 0.001,
+  longitudeDelta: 0.001,
 };
