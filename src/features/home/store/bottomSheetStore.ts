6506290b95c51435712dd28a029caa35dbf9e767
nohseongbong <shtjdqhd@naver.com>
2023-06-18 16:25:56 +0900

    feat: 바텀 시트 인덱스 추가

--- src/features/home/store/bottomSheetStore.ts
+++ src/features/home/store/bottomSheetStore.ts
@@ -1,13 +1,19 @@
-import {makeAutoObservable} from 'mobx';
+import {makeAutoObservable, runInAction} from 'mobx';
 import {FocusedType} from '../constants/bottomSheetFocusedType';
 
 class BottomSheetStore {
   focusedType: FocusedType = FocusedType.CREATE;
+  bottomSheetIndex: number = 0;
   constructor() {
     makeAutoObservable(this);
   }
   setFocusedType = (type: FocusedType) => {
     this.focusedType = type;
+  };
+  setBottomSheetIndex = (index: number) => {
+    runInAction(() => {
+      this.bottomSheetIndex = index;
+    });
   };
 }
 
