d27a9b71a367edf4b4410ce7dc84176a41ad158f
nohseongbong <shtjdqhd@naver.com>
2023-07-01 13:49:29 +0900

    style : 컴스텀 모달 추가

---
+++ src/shared/components/custom-modal/CustomModal.tsx
@@ -0,0 +1,11 @@
+import Modal from 'react-native-modal';
+
+const CustomModal = (props: any) => {
+  return (
+    <Modal animationOut="fadeOut" animationIn="fadeIn" {...props}>
+      {props.children}
+    </Modal>
+  );
+};
+
+export default CustomModal;
