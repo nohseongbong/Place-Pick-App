530e506e7eb7c5745a17db15fcf647d37a4cee77
nohseongbong <shtjdqhd@naver.com>
2023-07-16 16:53:25 +0900

    feat: toast 추가

--- src/lib/toast/showToast.ts
+++ src/lib/toast/showToast.ts
@@ -22,3 +22,25 @@ export const showPlaceRemoveToast = () => {
     bottomOffset: ht(80),
   });
 };
+
+export const showCourseRemoveToast = () => {
+  Toast.show({
+    type: 'courseRemove',
+    text1: '코스를 삭제했습니다',
+    visibilityTime: 2000,
+    autoHide: true,
+    position: 'bottom',
+    bottomOffset: ht(80),
+  });
+};
+
+export const showCourseNameUpdateToast = () => {
+  Toast.show({
+    type: 'courseNameUpdate',
+    text1: '코스 이름을 수정했습니다',
+    visibilityTime: 2000,
+    autoHide: true,
+    position: 'bottom',
+    bottomOffset: ht(80),
+  });
+};
