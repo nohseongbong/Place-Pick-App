94f77b48191d936ece556d3cea4d9302f47c73c5
nohseongbong <shtjdqhd@naver.com>
2023-07-23 19:01:17 +0900

    feat: env 값 추가

--- env.d.ts
+++ env.d.ts
@@ -3,4 +3,8 @@ declare module '@env' {
   export const GOOGLE_MAP_IOS_API_KEY: string;
   export const GOOGLE_MAP_ANDROID_API_KEY: string;
   export const GOOGLE_PLACE_API_KEY: string;
+  export const GOOGLE_LOGIN_AOS_CLIENT_ID: string;
+  export const GOOGLE_LOGIN_IOS_CLIENT_ID: string;
+  export const APPLE_LOGIN_CLIENT_ID: string;
+  export const APPLE_LOGIN_REFIRECT_URL: string;
 }
