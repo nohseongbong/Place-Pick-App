0af9d6ba825afb6eb941c61006d4f2ec484a4180
nohseongbong <shtjdqhd@naver.com>
2023-08-06 16:41:28 +0900

    refactor: env api url 수정

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,5 @@
 declare module '@env' {
-  export const TEST_URL_API: string;
+  export const PLACE_PICK_API_URL: string;
   export const GOOGLE_MAP_IOS_API_KEY: string;
   export const GOOGLE_MAP_ANDROID_API_KEY: string;
   export const GOOGLE_PLACE_API_KEY: string;
