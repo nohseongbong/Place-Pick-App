9403eb6fa5a3559413a02bb2f6dbc400b89fbc2a
nohseongbong <shtjdqhd@naver.com>
2023-08-10 22:28:03 +0900

    feat: create course api 추가

--- src/features/create-course/store/CourseDetailStore.ts
+++ src/features/create-course/store/CourseDetailStore.ts
@@ -1,4 +1,6 @@
 import {makeAutoObservable} from 'mobx';
+import {courseApi} from '../../../shared/api/course/api';
+import {courseStore} from '../../home/store/courseStore';
 
 class CourseDetailStore {
   isCourseNameModal: boolean = false;
@@ -13,6 +15,31 @@ class CourseDetailStore {
   };
   setCourseName = (name: string) => {
     this.courseName = name;
+  };
+
+  fetchCreateCourse = async (successFnc: () => void) => {
+    try {
+      const {data} = await courseApi.createCourse({
+        name: this.courseName,
+        courseLocationRequestsList: courseStore.courseList.map((item, index) => {
+          const {longitude, latitude} = item.location;
+          return {
+            longitude,
+            latitude,
+            placeId: item.place_id,
+            category: item.category,
+            placeName: item.name,
+            locationOrder: index + 1,
+          };
+        }),
+      });
+      console.log(data, ':fetchCreateCourse  data');
+      successFnc();
+    } catch (error) {
+      console.log(error, ':fetchCreateCourse  error');
+    } finally {
+      this.setIsCourseNameModal(false);
+    }
   };
 
   resetCourseDetail = () => {
