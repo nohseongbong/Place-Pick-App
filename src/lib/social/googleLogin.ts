5cb7a4fb738775565e888c464cdf2bb7da584565
nohseongbong <shtjdqhd@naver.com>
2023-09-03 15:51:07 +0900

    feat: 구글 id 토큰 값

--- src/lib/social/googleLogin.ts
+++ src/lib/social/googleLogin.ts
@@ -1,10 +1,12 @@
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 export const handleGoogleLogin = async () => {
+  GoogleSignin.configure({webClientId: '667768264255-v5v5ipdpaleo3rib40nboldvep7k5n8p.apps.googleusercontent.com'});
   try {
     await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
     await GoogleSignin.signIn();
-    const {accessToken} = await GoogleSignin.getTokens();
+    const {accessToken, idToken} = await GoogleSignin.getTokens();
+    console.log(idToken, ': 구글 로그인 idToken ');
     console.log(accessToken, ': 구글 로그인 ');
   } catch (error) {
     console.log(error, ' : 구글 로그인 에러');
