ef65445c8c7bb91aa8e6fedd7a2ab3d174515b0b
nohseongbong <shtjdqhd@naver.com>
2023-09-05 17:21:26 +0900

    feat: 카카오 공유하기 기능 추가

--- src/screens/CourseDetailScreen.tsx
+++ src/screens/CourseDetailScreen.tsx
@@ -45,13 +45,13 @@ const CourseDetailScreen = observer(() => {
     try {
       const response = await KakaoShareLink.sendFeed({
         content: {
-          title: 'title',
+          title: '노성봉님께서 데이트 코스를 공유했습니다.',
           imageUrl: 'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg',
           link: {
             webUrl: 'placepick://',
             mobileWebUrl: 'placepick://',
           },
-          description: 'description',
+          description: courseDetailStore.courseName,
         },
         buttons: [
           {
