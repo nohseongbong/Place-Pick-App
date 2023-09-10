43d8ee2c48dd88bfb1c7f9e9d4f9cd1392e8a084
nohseongbong <shtjdqhd@naver.com>
2023-09-10 18:56:50 +0900

    feat: 코스 맵 라인 및 줌 설정 완료

--- src/shared/components/course/components/CourseMapView.tsx
+++ src/shared/components/course/components/CourseMapView.tsx
@@ -1,28 +1,59 @@
 import React from 'react';
 import {View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
-import {NavigationProp, useNavigation} from '@react-navigation/native';
+import MapView, {PROVIDER_GOOGLE, Marker, Polyline} from 'react-native-maps';
 import {mapStyle} from '../../../../features/home/constants/mapStyle';
 import style from '../styles/courseMapView';
+import {CourseType} from '../../../types/place/placeType';
+import {CourseCategoryIconView} from '../../category-icon/CategoryIcon';
+import CustomText from '../../customComponents/CustomText';
+import {centerZoom} from '../../../utils/locationCenter';
+import {palette} from '../../../constants/palette';
 
-const CourseMapView = () => {
+type Props = {
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+  courseList: CourseType[];
+};
+
+const initialRegion = {
+  latitude: 37.4979052,
+  longitude: 127.0275777,
+  latitudeDelta: 0.001,
+  longitudeDelta: 0.001,
+};
+
+const CourseMapView = ({location, courseList}: Props) => {
   const styles = style();
-  const initialRegion = {
-    latitude: 37.4979052,
-    longitude: 127.0275777,
-    latitudeDelta: 0.001,
-    longitudeDelta: 0.001,
+  const region = {
+    ...location,
+    ...centerZoom(courseList),
   };
 
+  const markerCoordinates = courseList.map(course => course.location);
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map_wrap}
         customMapStyle={mapStyle}
         provider={PROVIDER_GOOGLE}
-        // region={{...homeStore.mapLocation}}
-        // onRegionChangeComplete={getMapCenter}
-        initialRegion={initialRegion}></MapView>
+        region={region}
+        initialRegion={initialRegion}>
+        {courseList.map((course, index) => {
+          return (
+            <Marker coordinate={course.location} key={course.place_id} id={course.place_id}>
+              <View style={styles.marker_container}>
+                <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                  <CourseCategoryIconView type={course.category} width={28} />
+                  <CustomText style={styles.marker_num}>{index + 1}</CustomText>
+                </View>
+              </View>
+            </Marker>
+          );
+        })}
+        <Polyline coordinates={markerCoordinates} strokeColor={palette.PRIMARY} strokeWidth={4} />
+      </MapView>
     </View>
   );
 };
