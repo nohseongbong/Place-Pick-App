0c16ca368a1051edd423acd51521d99d53086f00
nohseongbong <shtjdqhd@naver.com>
2023-09-24 18:44:21 +0900

    feat: login api 추가

--- src/shared/api/auth/api.ts
+++ src/shared/api/auth/api.ts
@@ -1,8 +1,17 @@
 import {axiosInstance} from '..';
+import {LoginReq} from './types/requestType';
 import {LoginRes, ResType} from './types/responseType';
 
 export class Api {
-  login = (): Promise<ResType<LoginRes>> => axiosInstance.post('/login');
+  login = async ({
+    token,
+    providerType,
+  }: LoginReq): Promise<ResType<LoginRes>> => {
+    const {data} = await axiosInstance.post('/auth/social/login', {
+      token,
+      providerType,
+    });
+    return data.data;
+  };
 }
-
 export const authApi = new Api();
