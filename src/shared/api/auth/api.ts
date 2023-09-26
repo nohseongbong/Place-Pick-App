34aead2ad12a08fc8df4d1edef18a68ee69321ea
nohseongbong <shtjdqhd@naver.com>
2023-09-26 17:26:32 +0900

    refactor: login api type 수정

--- src/shared/api/auth/api.ts
+++ src/shared/api/auth/api.ts
@@ -1,17 +1,14 @@
 import {axiosInstance} from '..';
 import {LoginReq} from './types/requestType';
-import {LoginRes, ResType} from './types/responseType';
+import {LoginRes} from './types/responseType';
 
 export class Api {
-  login = async ({
-    token,
-    providerType,
-  }: LoginReq): Promise<ResType<LoginRes>> => {
+  login = async ({token, providerType}: LoginReq): Promise<LoginRes> => {
     const {data} = await axiosInstance.post('/auth/social/login', {
       token,
       providerType,
     });
-    return data.data;
+    return data.body.token;
   };
 }
 export const authApi = new Api();
