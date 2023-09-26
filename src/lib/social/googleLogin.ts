62c7eca667590d3e1146009c76b0205c80c9981a
nohseongbong <shtjdqhd@naver.com>
2023-09-26 17:26:04 +0900

    feat: google login 추가

--- src/lib/social/googleLogin.ts
+++ src/lib/social/googleLogin.ts
@@ -1,15 +1,19 @@
 import {GOOGLE_LOGIN_AOS_CLIENT_ID, GOOGLE_LOGIN_IOS_CLIENT_ID} from '@env';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import {Platform} from 'react-native';
+import {authStore} from '../../shared/store/authStore';
 
 export const handleGoogleLogin = async () => {
-  const clientId = Platform.OS === 'android' ? GOOGLE_LOGIN_AOS_CLIENT_ID : GOOGLE_LOGIN_IOS_CLIENT_ID;
+  const clientId =
+    Platform.OS === 'android'
+      ? GOOGLE_LOGIN_AOS_CLIENT_ID
+      : GOOGLE_LOGIN_IOS_CLIENT_ID;
   GoogleSignin.configure({webClientId: clientId});
   try {
     await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
     await GoogleSignin.signIn();
     const data = await GoogleSignin.getTokens();
-    console.log(data, ': 구글 로그인 idToken ');
+    authStore.login({token: data.idToken, providerType: 'GOOGLE'});
   } catch (error) {
     console.log(error, ' : 구글 로그인 에러');
   }
