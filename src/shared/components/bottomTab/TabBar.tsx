d07063c10bb1e0f3cd464492699abb822fc6527d
nohseongbong <shtjdqhd@naver.com>
2023-10-06 17:06:31 +0900

    feat: 게스트 모드 시 컬렉션 스크린 조건 추가

--- src/shared/components/bottomTab/TabBar.tsx
+++ src/shared/components/bottomTab/TabBar.tsx
@@ -9,74 +9,94 @@ import {SVG_IMG} from '../../../assets/images';
 import CustomText from '../customComponents/CustomText';
 import {collectionStore} from '../../../features/collection/store/collectionStore';
 import DeleteBar from '../../../features/collection/components/collection-list/DeleteBar';
+import {authStore} from '../../store/authStore';
 
-const TabBar = observer(({state, descriptors, navigation}: BottomTabBarProps) => {
-  const styles = style();
+const TabBar = observer(
+  ({state, descriptors, navigation}: BottomTabBarProps) => {
+    const styles = style();
 
-  return collectionStore.isEdit ? (
-    <View style={{alignItems: 'center', position: 'absolute', bottom: 20, width: '100%'}}>
-      <DeleteBar />
-    </View>
-  ) : (
-    <View style={styles.container}>
-      {state.routes.map((route, index) => {
-        const {options} = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
+    return collectionStore.isEdit ? (
+      <View
+        style={{
+          alignItems: 'center',
+          position: 'absolute',
+          bottom: 20,
+          width: '100%',
+        }}>
+        <DeleteBar />
+      </View>
+    ) : (
+      <View style={styles.container}>
+        {state.routes.map((route, index) => {
+          const {options} = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+              ? options.title
+              : route.name;
 
-        const isFocused = state.index === index;
+          const isFocused = state.index === index;
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
+            if (!authStore.accessToken) {
+              authStore.setIsLoginModal(true);
+              return;
+            }
 
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name);
+            }
+          };
 
-        return (
-          <CustomTouchable
-            key={route.key}
-            activeOpacity={1}
-            accessibilityRole="button"
-            accessibilityState={isFocused ? {selected: true} : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={styles.icon}>
-            {label === '코스 만들기' ? (
-              isFocused ? (
-                <SVG_IMG.HOME_ON width={20} height={20} />
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
+
+          return (
+            <CustomTouchable
+              key={route.key}
+              activeOpacity={1}
+              accessibilityRole="button"
+              accessibilityState={isFocused ? {selected: true} : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={styles.icon}>
+              {label === '코스 만들기' ? (
+                isFocused ? (
+                  <SVG_IMG.HOME_ON width={20} height={20} />
+                ) : (
+                  <SVG_IMG.HOME_OFF width={20} height={20} />
+                )
+              ) : isFocused ? (
+                <SVG_IMG.COLLECTION_ON width={20} height={20} />
               ) : (
-                <SVG_IMG.HOME_OFF width={20} height={20} />
-              )
-            ) : isFocused ? (
-              <SVG_IMG.COLLECTION_ON width={20} height={20} />
-            ) : (
-              <SVG_IMG.COLLECTION_OFF width={20} height={20} />
-            )}
-            <CustomText style={[styles.icon_text, !isFocused && {color: palette.BORDER}]}>{String(label)}</CustomText>
-          </CustomTouchable>
-        );
-      })}
-    </View>
-  );
-});
+                <SVG_IMG.COLLECTION_OFF width={20} height={20} />
+              )}
+              <CustomText
+                style={[
+                  styles.icon_text,
+                  !isFocused && {color: palette.BORDER},
+                ]}>
+                {String(label)}
+              </CustomText>
+            </CustomTouchable>
+          );
+        })}
+      </View>
+    );
+  },
+);
 
 export default TabBar;
