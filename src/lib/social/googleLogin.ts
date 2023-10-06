f1ffdfe7ddd674954b87102fc876c91b09b7267b
nohseongbong <shtjdqhd@naver.com>
2023-10-06 17:06:54 +0900

    feat: login action 추가

--- src/lib/social/googleLogin.ts
+++ src/lib/social/googleLogin.ts
@@ -3,7 +3,11 @@ import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import {Platform} from 'react-native';
 import {authStore} from '../../shared/store/authStore';
 
-export const handleGoogleLogin = async () => {
+type Props = {
+  action: () => void;
+};
+
+export const handleGoogleLogin = async ({action}: Props) => {
   const clientId =
     Platform.OS === 'android'
       ? GOOGLE_LOGIN_AOS_CLIENT_ID
@@ -13,7 +17,7 @@ export const handleGoogleLogin = async () => {
     await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
     await GoogleSignin.signIn();
     const data = await GoogleSignin.getTokens();
-    authStore.login({token: data.idToken, providerType: 'GOOGLE'});
+    authStore.login({token: data.idToken, providerType: 'GOOGLE', action});
   } catch (error) {
     console.log(error, ' : 구글 로그인 에러');
   }
