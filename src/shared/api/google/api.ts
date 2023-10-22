8541e548f3efef71560f1fac7dc1ca93b3522612
nohseongbong <shtjdqhd@naver.com>
2023-10-22 17:25:18 +0900

    style: 프리티어 수정으로 인해 코드 스타일 변경

--- src/shared/api/google/api.ts
+++ src/shared/api/google/api.ts
@@ -1,9 +1,20 @@
 import {axiosInstanceGoogleApi} from '..';
-import {GooglePlaceDetailReq, GooglePlaceListReq, GooglePlaceSearchReq} from './types/requestType';
-import {GooglePlaceDetailRes, GooglePlaceListRes, GooglePlaceSearchRes} from './types/responseType';
+import {
+  GooglePlaceDetailReq,
+  GooglePlaceListReq,
+  GooglePlaceSearchReq,
+} from './types/requestType';
+import {
+  GooglePlaceDetailRes,
+  GooglePlaceListRes,
+  GooglePlaceSearchRes,
+} from './types/responseType';
 
 export class Api {
-  getGooglePlaceList = ({location, category}: GooglePlaceListReq): Promise<GooglePlaceListRes> =>
+  getGooglePlaceList = ({
+    location,
+    category,
+  }: GooglePlaceListReq): Promise<GooglePlaceListRes> =>
     axiosInstanceGoogleApi.get('/place/nearbysearch/json', {
       params: {
         location: `${location.latitude},${location.longitude}`,
@@ -12,14 +23,20 @@ export class Api {
         type: category,
       },
     });
-  getGooglePlaceDetail = ({place_id}: GooglePlaceDetailReq): Promise<GooglePlaceDetailRes> =>
+  getGooglePlaceDetail = ({
+    place_id,
+  }: GooglePlaceDetailReq): Promise<GooglePlaceDetailRes> =>
     axiosInstanceGoogleApi.get('/place/details/json', {
       params: {
         place_id: place_id,
-        fields: 'name,formatted_address,rating,user_ratings_total,url,place_id,types,geometry',
+        fields:
+          'name,formatted_address,rating,user_ratings_total,url,place_id,types,geometry',
       },
     });
-  searchGooglePlaces = ({query, type}: GooglePlaceSearchReq): Promise<GooglePlaceSearchRes> =>
+  searchGooglePlaces = ({
+    query,
+    type,
+  }: GooglePlaceSearchReq): Promise<GooglePlaceSearchRes> =>
     axiosInstanceGoogleApi.get('/place/textsearch/json', {
       params: {
         query: query,
